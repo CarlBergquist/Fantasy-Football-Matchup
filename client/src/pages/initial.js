ba8cab3797e433577860a724c69bf19ed1b67631
Carl Bergquist <carlbergquist81@gmail.com>
2022-12-08 19:29:55 -0600

    bootstrap

---
+++ client/src/pages/initial.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//import profilepic from '../../assets/me.jpeg';
+//import "../../styles/about.css"
+
+export default function Init() {
+    return (
+        <div className="border">
+            <h1>Fantasy Football Matchup Analyzer</h1>
+            <button>Login</button>
+            <button>Sign Up</button>
+        </div>
+    );
+}
