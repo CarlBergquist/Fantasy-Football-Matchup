41053126c99490702ec9374be51b3d77ca3c0cad
Jack Warrick <jackwarrick@Jacks-MacBook-Pro.local>
2022-12-08 20:54:09 -0600

    main.js and account.js draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
 import Init from './pages/initial'
+import Main from './pages/main';
+import Account from './pages/account';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -8,7 +10,10 @@ function App() {
       <>
       <Routes>
         <Route path='/' element={<Init />} />
+        <Route path='/main' element={<Main />} />  
+        <Route path='/account' element={<Account />} />  
       </Routes>
+
       </>
     </Router>
   );
---
+++ client/src/pages/main.js
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_MATCHUPS } from '../utils/queries';
+
+
+
+//This was pulled from the week 21 mini project
+const Main = () => {
+    const { loading, data } = useQuery(QUERY_MATCHUPS, {
+        fetchPolicy: "no-cache"
+    })
+
+    //This was pulled from week 21 mini project - will not be exactl what we use to insert the data
+    const matchupList = data?.matchups || [];
+
+return (
+<div className="container">
+<h1>All Matchups</h1>   
+   {matchupList.map((matchup) => {  
+    return (
+    <div className= "card bg-gray w-75 d-flex flex-row">
+        <h6>Matchup created by {}</h6>
+        <div className= "">
+            <image>{matchup.player1.image}</image>
+            <h3>{matchup.player1.name}</h3>
+            <button className= "text-dark bg-green" input="">Vote</button>
+            <h3>{Math}%</h3>
+
+        </div>
+
+        <div className="text-center">
+        <h2>VS</h2>
+        </div>
+
+        <div className= "">
+            <image>{matchup.player2.image}</image>
+            <h3>{matchup.player2.name}</h3>
+            <button className= "text-dark bg-green" input="">Vote</button>
+            <h3>{Math}%</h3>
+
+        </div>
+
+    </div>
+    );
+    })};
+
+ </div>
+)
+
+}
+
+export default Main;
