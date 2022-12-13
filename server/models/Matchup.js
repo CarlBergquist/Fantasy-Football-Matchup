fd293c1e1a520f93c6a4fc979474d6915c9765e5
hrcoop16 <hrcoop16@gmail.com>
2022-12-12 20:20:40 -0600

    added models

---
+++ server/models/Matchup.js
@@ -0,0 +1,24 @@
+const { Schema, model } = require("mongoose");
+
+const matchupSchema = new Schema({
+  player1: {
+    type: String,
+    required: true,
+  },
+  player2: {
+    type: String,
+    required: true,
+  },
+  player1_votes: {
+    type: Number,
+    default: 0,
+  },
+  player2_votes: {
+    type: Number,
+    default: 0,
+  },
+});
+
+const Matchup = model("Matchup", matchupSchema);
+
+module.exports = Matchup;
