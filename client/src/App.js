40c8718dcf81dbd0f78172a8fae845392f865cf9
hrcoop16 <hrcoop16@gmail.com>
2022-12-12 19:20:40 -0600

    added NavBAr

--- client/src/App.js
+++ client/src/App.js
@@ -1,21 +1,22 @@
-import Init from './pages/initial'
-import Main from './pages/main';
-import Account from './pages/account';
-import './App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Init from "./pages/initial";
+import Main from "./pages/main";
+import Account from "./pages/account";
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <>
-      <Routes>
-        <Route path='/' element={<Init />} />
-        <Route path='/main' element={<Main />} />  
-        <Route path='/account' element={<Account />} />  
-      </Routes>
-
-      </>
-    </Router>
+    <div>
+      <Router>
+        <>
+          <Routes>
+            <Route path="/" element={<Init />} />
+            <Route path="/main" element={<Main />} />
+            <Route path="/account" element={<Account />} />
+          </Routes>
+        </>
+      </Router>
+    </div>
   );
 }
 
--- client/src/pages/initial.js
+++ client/src/pages/initial.js
@@ -1,13 +1,15 @@
-import React from 'react';
+import React from "react";
+import Nav from "../components/nav";
 //import profilepic from '../../assets/me.jpeg';
 //import "../../styles/about.css"
 
 export default function Init() {
-    return (
-        <div className="">
-            <h1>Fantasy Football Matchup Analyzer</h1>
-            <button>Login</button>
-            <button>Sign Up</button>
-        </div>
-    );
+  return (
+    <div className="">
+      <Nav />
+      <h1>Fantasy Football Matchup Analyzer</h1>
+      <button>Login</button>
+      <button>Sign Up</button>
+    </div>
+  );
 }
