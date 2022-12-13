00698adcbf2da3db4871eb55c5f24dd1e54f35e2
Jack Warrick <jackwarrick@Jacks-MBP.zyxel.com>
2022-12-12 19:05:00 -0600

    matchup.js file done

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import Init from './pages/initial'
 import Main from './pages/main';
 import Account from './pages/account';
+import Matchup from './pages/matchup';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
         <Route path='/' element={<Init />} />
         <Route path='/main' element={<Main />} />  
         <Route path='/account' element={<Account />} />  
+        <Route path='/matchup' element={<Matchup />} />  
       </Routes>
 
       </>
