3cd786e2f3697fd8d4dd32667b4f3e3b34db093f
Carl Bergquist <carlbergquist81@gmail.com>
2022-12-19 22:00:30 -0600

    link

--- client/src/pages/initial.js
+++ client/src/pages/initial.js
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "../components/footer";
 //import profilepic from '../../assets/me.jpeg';
 //import "../../styles/about.css"
+import { Link } from 'react-router-dom'
 
 export default function Init() {
 
@@ -9,8 +10,8 @@ export default function Init() {
         <div className="m-2">
             <h1 className="display-1 d-flex justify-content-center">Fantasy Football Matchup Analyzer</h1>
             <div className="d-flex justify-content-center">
-            <a href='./login'className="m-2" ><button className="btn btn-primary">Login</button></a>
-            <a href='./signup'className="m-2"><button className="btn btn-primary">Sign Up</button></a>
+            <Link to='/login'className="m-2" ><button className="btn btn-primary">Login</button></ Link>
+            <Link to='/signup'className="m-2"><button className="btn btn-primary">Sign Up</button></Link>
             </div>
             <img src="https://static.www.nfl.com/image/private/t_q-best/league/pyzxubxdovaubclvbvf0" className="img-fluid max-width: 40% height: auto"></img>
             <Footer />
